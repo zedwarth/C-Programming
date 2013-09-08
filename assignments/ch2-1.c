ea2059233424390ff563c1984f51652f32ee4a46
zedwarth <zedwarth@gmail.com>
2013-09-08 11:55:49 -0400

    Created skeleton template for homework assignments and first draft of first assignment.

---
+++ assignments/ch2-1.c
@@ -0,0 +1,35 @@
+/* Seth Szymanski
+ * gcc (GCC) 4.8.1 20130725 (prerelease)
+ * Chapter 2, Assignment 1
+ * input two integers and output the larger
+ * */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main ( void )
+{
+	//Initialise variables
+	int integer1, integer2, largest;
+
+	printf("Enter an integer:"); 
+	scanf("%d", &integer1);
+
+	printf("Enter another integer:"); 
+	scanf("%d", &integer2);
+
+	if (integer1 == integer2) {
+		printf("The integers are equal.\n");
+	}
+	else{
+		if (integer1 > integer2) {
+			largest = integer1;
+		}
+		if (integer1 < integer2) {
+			largest = integer2;
+		}
+	}
+
+	printf("The larger number is %d.\n", largest);
+
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
---
+++ assignments/skel.cpp
@@ -0,0 +1,11 @@
+/* Seth Szymanski
+ * Chapter *, Assignment *
+ * gcc (GCC) 4.8.1 20130725 (prerelease)
+ * */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main ( void )
+{
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
