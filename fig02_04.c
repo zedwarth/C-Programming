da4df6694b55150f672ac16d710a72df3ca51532
zedwarth <zedwarth@gmail.com>
2013-09-08 09:22:46 -0400

    Figures 02_03 through 02_05 added.  skel.c created as a template.

---
+++ fig02_04.c
@@ -0,0 +1,10 @@
+/* Fig. 2.4: fig02_04.c
+ * Printing multiple lines with a single printf */
+
+/* function main begins program execution */
+int main ( void )
+{
+	printf( "Welcome\nto\nC!\n" );
+
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
---
+++ skel.c
@@ -0,0 +1,9 @@
+/* Fig. .: fig0_0.c
+ * */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main ( void )
+{
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
