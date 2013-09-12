3e8e823bfaf906462948710c2d59dd3cb42c47e5
zedwarth <zedwarth@gmail.com>
2013-09-12 18:09:29 -0400

    Added two book examples: 'fig03_06.c' and 'fig03_08.c'.

---
+++ fig03_06.c
@@ -0,0 +1,30 @@
+/* Fig. 3.6: fig03_06.c
+ * Class average program with counter-controlled repetition  */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main ( void )
+{
+	int counter; /* number of grade to be entered next */
+	int grade; /* grade value */
+	int total; /* sum of grades input by user */
+	int average; /* average of grades */
+
+	/* initialization phase */
+	total = 0; /* initialize total */
+	counter = 1; /* initialize loop counter */
+
+	/* processing phase */
+	while ( counter <= 10 ) { /* loop 10 times */
+		printf( "Enter grade: " ); /* prompt for input */
+		scanf( "%d", &grade ); /* read prompt from user */
+		total = total + grade; /* add grade to total */
+		counter = counter + 1; /* increment counter */
+	} /* end while */
+
+	/* termination phase */
+	average = total / 10; /* integer division */
+
+	printf( "Class average is %d\n", average ); /* display results */
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
