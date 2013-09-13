1e59360e6690fe54ab9f042b06720b5b590466d6
zedwarth <zedwarth@gmail.com>
2013-09-13 13:11:00 -0400

    Added decoding.  Program in working state.

--- assignments/ch3.cpp
+++ assignments/ch3.cpp
@@ -29,15 +29,27 @@ int main ( void )
 	printf("%d\n", hundreds);
 	printf("%d\n", thousands);
 */
+	//Encode
 	if (mode == 1) { //encode
 		ones = (ones + 7) % 10;
 		tens = (tens + 7) % 10;
 		hundreds = (hundreds + 7) % 10;
 		thousands = (thousands + 7) % 10;
-		printf("%d", tens);
-		printf("%d", ones);
-		printf("%d", thousands);
-		printf("%d\n", hundreds);
 	}
+
+	//Decode
+	if (mode == 2) {
+		ones = (ones + 3) % 10;
+		tens = (tens + 3) % 10;
+		hundreds = (hundreds + 3) % 10;
+		thousands = (thousands + 3) % 10;
+	}
+
+	//Print swapped digits
+	printf("%d", tens);
+	printf("%d", ones);
+	printf("%d", thousands);
+	printf("%d\n", hundreds);
+
 	return 0; /*indicate that program ended successfully */
 } /* end function main */
