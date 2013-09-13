7fb3588d44240f42d8f1b546a3651970b6aa04cb
zedwarth <zedwarth@gmail.com>
2013-09-13 15:15:04 -0400

    Error messages for invalid input on option prompts.

--- assignments/ch3.cpp
+++ assignments/ch3.cpp
@@ -1,59 +1,66 @@
 /* Seth Szymanski
- * Chapter 3 Assignment
- * (GCC) 4.2.1
- * Write an encryption and decryption program.
- * */
+* Chapter 3 Assignment
+* (GCC) 4.2.1
+* Write an encryption and decryption program.
+* */
 #include <stdio.h>
 
 /* function main begins program execution */
 int main ( void )
 {
+	//Declare Variables 
 	int input, mode, sentinel, ones, tens, hundreds, thousands;
-	mode = sentinel = 0;
 
 	do {
-
+		//Prompt for mode
 		printf("Encode (1) Decode (2) : ");
 		scanf("%d", &mode);
+		if (mode != 1 && mode != 2) {
+			printf("That is not a valid option.\n");
+			sentinel = 1; 
+	}
 
-		printf("Enter a  four digit number: ");
-		scanf("%d", &input);
+		else {
+			//Input numbers
+			printf("Enter a  four digit number: ");
+			scanf("%d", &input);
 
-		ones = input % 10;
-		input /= 10;
-		tens = input % 10;
-		input /= 10;
-		hundreds = input % 10;
-		input /= 10;
-		thousands = input;
-		
-		//Encode
-		if (mode == 1) { //encode
+			//Place each digit in a variable
+			ones = input % 10;
+			input /= 10;
+			tens = input % 10;
+			input /= 10;
+			hundreds = input % 10;
+			input /= 10;
+			thousands = input;
+
+			//Encode
+			if (mode == 1) { //encode
 			ones = (ones + 7) % 10;
 			tens = (tens + 7) % 10;
 			hundreds = (hundreds + 7) % 10;
 			thousands = (thousands + 7) % 10;
 			printf("Encoded Digits: ");
-		}
+			}
 
-		//Decode
-		if (mode == 2) {
+			//Decode
+			else if (mode == 2) {
 			ones = (ones + 3) % 10;
 			tens = (tens + 3) % 10;
 			hundreds = (hundreds + 3) % 10;
 			thousands = (thousands + 3) % 10;
 			printf("Decoded Digits: ");
+	}
+			//Print swapped digits
+			printf("%d", tens);
+			printf("%d", ones);
+			printf("%d", thousands);
+			printf("%d\n", hundreds);
+
+			//Continue or Exit; Assume Exit
+			printf("Continue (1) Exit (0): ");
+			scanf("%d", &sentinel);
 		}
-
-		//Print swapped digits
-		printf("%d", tens);
-		printf("%d", ones);
-		printf("%d", thousands);
-		printf("%d\n", hundreds);
-
-		//Countinue or Exit; Assume Exit
-		printf("Continue (1) Exit (0): ");
-		scanf("%d", &sentinel);
 
 	}while (sentinel == 1);
 
