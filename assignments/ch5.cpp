01629f533faad3c8c61531066439ea20e39e287d
zedwarth <zedwarth@gmail.com>
2013-10-03 17:05:37 -0400

    Chatper 5 assignment completed.

--- assignments/ch5.cpp
+++ assignments/ch5.cpp
@@ -1,5 +1,5 @@
 /* Seth Szymanski
- * Chapter 5 Assignment 
+ * Chapter 5 Assignment
  * clang version 3.3
  * Coin flip simulation
  * */
@@ -7,44 +7,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-int flipCoin( int );
+int flipCoin( void );
 
 /* function main begins program execution */
 int main( void )
 {
-	do { 
-	int trials = 10;
+	int sentinal = 1;
+	int i, toss, flip;
+	srand( time( NULL ) );
 
-		while (trails <= 1000000){
-	int heads = 0;
-	int tails = 0;
-			for( count = trails; count  >= 0; count--) {
-				if ( flipCoin == 0 ) {
-					heads++;
-				}
-				else {
-					tails++
-				}
-				printf("Trials: %d\n", count);
-				printf("Head count: %9d Percent Heads %2.2f\n", heads, float(heads/count);
-				printf("Tail count: %9d Percent Tails %2.2f\n", tails, float(tails/count);
-				}
-				}
+	do{
+		for( toss = 10; toss <= 1000000; toss = toss * 10 ){
+			int heads = 0;
+			int tails = 0;
+			
 
-	} while countinue == 1;
+			for( i = toss; i > 0; i-- ){
+				flip = flipCoin();
+				if( flip == 0) heads++;
+				else tails++;
+			}
 
+			printf("Trials: %d\n", toss);
+			printf("Head count: %9d Percent Heads %2.2f\n", heads, double(heads)/toss*100);
+			printf("Tail count: %9d Percent Tails %2.2f\n", tails, double(tails)/toss*100);
+		}
+
+		printf("Try Again (1) Exit (0)");
+		scanf("%d", &sentinal);
+		
+	}while(sentinal == 1);
 	return 0; /*indicate that program ended successfully */
 } /* end function main */
 
-int flipCoin( count );
+int flipCoin( void )
 {
-	//Seed from System Clock
-	srand( time( NULL ) );
-	
-	//Return 0 or 1
-	return ( rand() % 2); 
+
+    //Return 0 or 1
+    return ( rand() % 2);
 
 //	return tails; //Source from a real coin flip; guaranteed to be random
 }
-
-
