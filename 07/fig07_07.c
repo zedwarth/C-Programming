a0ad1c41bfd3e63d96b99d4ec030da93196144bd
zedwarth <zedwarth@gmail.com>
2013-10-07 17:27:34 -0400

    add fig07_06.c fig07_07.c

---
+++ 07/fig07_07.c
@@ -0,0 +1,25 @@
+/* Fig. 7.x: fig07_0x.c
+ * Cube a variable using call-by-reference with a pointer argument */
+
+#include <stdio.h>
+
+void cubeByReference( int *nPtr ); /* prototype */
+
+int main ( void )
+{
+	int number = 5; /* initialize number */
+
+	printf( "The original value of number is %d", number );
+
+	/* pass address of number to cubeByReference */
+	cubeByReference( &number );
+
+	printf( "\nThe new value of number is %d\n", number );
+	return 0; /*indicate successful termination */
+} /* end main */
+
+/* calculate cube of *nPtr; modifies variable number in main */
+void cubeByReference( int *nPtr )
+{
+	*nPtr = *nPtr * *nPtr * *nPtr; /* cube *nPtr */
+} /* end function cubeByReference */
