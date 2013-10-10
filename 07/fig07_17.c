70daf517e72f4cccd25bb6bbf27bb1de8576d265
zedwarth <zedwarth@gmail.com>
2013-10-09 22:05:04 -0400

    add fig07_17.c

---
+++ 07/fig07_17.c
@@ -0,0 +1,31 @@
+/* Fig. 7.17: fig07_17.c
+ * Demonstrating the sizeof operator */
+#include <stdio.h>
+
+int main ( void )
+{
+	char c;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	long double ld;
+	int array[ 20 ]; /* create array of 20 int elements */
+	int *ptr = array; /* create pointer to array */
+
+	printf( "    sizeofc c = %lu\tsizeof(char)  = %lu"
+		"\n     sizeof s = %lu\tsizeof(short) = %lu"
+		"\n     sizeof i = %lu\tsizeof(int) = %lu"
+		"\n     sizeof l = %lu\tsizeof(long) = %lu"
+		"\n     sizeof f = %lu\tsizeof(float) = %lu"
+		"\n     sizeof d = %lu\tsizeof(double) = %lu"
+		"\n    sizeof ld = %lu\tsizeof(long double) = %lu"
+		"\n sizeof array = %lu"
+		"\n   sizeof ptr = %lu\n",
+		sizeof c, sizeof( char ), sizeof s, sizeof( short ), sizeof i,
+		sizeof( int ), sizeof l, sizeof( long ), sizeof f,
+		sizeof( float ), sizeof d, sizeof( double ), sizeof ld,
+		sizeof( long double ), sizeof array, sizeof ptr );
+	return 0; /*indicate successful termination */
+} /* end main */
