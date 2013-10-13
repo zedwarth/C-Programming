9b3b9cba566a0e0fca910027662ec86540d89c4b
zedwarth <zedwarth@gmail.com>
2013-10-12 22:23:01 -0400

    ch7 assignment completed

--- assignments/ch7.cpp
+++ assignments/ch7.cpp
@@ -4,24 +4,30 @@
  * Show what the 'mystery' function does.
  * */
 #include <stdio.h>
+#include <stdlib.h>
 
 int mystery( const char *s1, const char *s2 ); //prototype
 
-/* function main begins program execution */
 int main ( void )
 {
 	char string1[28] = "Once upon a midnight dreary";
 	char string2[28] = "Once upon a midnight dreary";
-
-	printf( "string1 is:\n%s\n\n string2 is:\n%s\n\n", string1, string2 );
-	printf( "When two identical strings are past, mystery returns: %d\n",
-			mystery( string1, string2 ) );
-
 	char string3[63] = "Once upon a midnight dreary, while I pondered weak and weary,";
-	printf( "string1 is:\n%s\n\n string3 is:\n%s\n\n", string1, string3 );
-	printf( "When two strings are identical up until the length of the smaller string, mystery also returns: %d\n", mystery( string1, string3 ) );
+	char string4[58] = "Over many a quaint and curious volume of forgotten lore,";
 
+	printf( "When two identical strings are past, mystery returns: 1\n" );
+	printf( "Example:\nstring1 is: %s\nstring2 is: %s\n", string1, string2 );
+	printf( "mystery( string1, string2 ) %d\n\n", mystery( string1, string2 ) );
 
+	printf( "When two strings are of different lengths but identical up until the\nend of the smaller string, mystery returns: 1\n" );
+	printf( "Example:\nstring1 is: %s\nstring3 is: %s\n", string1, string3 );
+	printf( "mystery( string1, string3 ) %d\n\n", mystery( string1, string3 ) );
+
+	printf( "When two strings are different, mystery returns: 0\n");
+	printf( "Example:\nstring1 is: %s\nstring4 is: %s\n", string1, string4 );
+	printf( "mystery( string1, string4 ) %d\n\n", mystery( string1, string4 ) );
+
+	system("pause");
 	return 0; /*indicate that program ended successfully */
 } /* end function main */
 
