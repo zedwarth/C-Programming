70d16138a4ab7a83edd5ee447944b3ece2e3b544
zedwarth <zedwarth@gmail.com>
2013-10-19 18:25:40 -0400

    update skel.cpp

--- assignments/skel.cpp
+++ assignments/skel.cpp
@@ -4,9 +4,11 @@
  * Description
  * */
 #include <stdio.h>
+#include <stdlib.h>
 
 /* function main begins program execution */
 int main ( void )
 {
+	system("pause");
 	return 0; /*indicate that program ended successfully */
 } /* end function main */
