2d0e079157490fd04b94ffc48ff15c4862b7a8b3
zedwarth <zedwarth@gmail.com>
2013-10-19 18:29:35 -0400

    start chapter 8

---
+++ assignments/ch8.cpp
@@ -0,0 +1,14 @@
+/* Seth Szymanski
+ * Chapter 8 Assignment 
+ * clang version 3.3
+ * Description
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function main begins program execution */
+int main ( void )
+{
+	system("pause");
+	return 0; /*indicate that program ended successfully */
+} /* end function main */
