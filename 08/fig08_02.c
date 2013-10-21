d4ed0f1737b47e39008e9956ab7dba711d93221a
zedwarth <zedwarth@gmail.com>
2013-10-21 16:29:49 -0400

    fig08_02.c is working now

--- 08/fig08_02.c
+++ 08/fig08_02.c
@@ -5,8 +5,8 @@
 
 int main ( void )
 {
-	printf( "%s\n%s%s\n%s%s\n\n", "According to isdigit: ",	
-		isdigit( "8" ) ? "8 is a " : "8 is not a ", "digit",
+	printf( "%s\n%s%s\n%s%s\n\n", "According to isdigit: ",
+		isdigit( '8' ) ? "8 is a " : "8 is not a ", "digit",
 		isdigit( '#' ) ? "# is a " : "# is not a ", "digit" );
 
 	printf( "%s\n%s%s\n%s%s\n%s%s\n%s%s\n\n",
