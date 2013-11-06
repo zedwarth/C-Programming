8686b397db2fe6f9edc76b690a9cd6a4c804ee58
zedwarth <zedwarth@gmail.com>
2013-11-06 15:14:48 -0500

    add fig10_02.c

---
+++ 10/fig10_02.c
@@ -0,0 +1,27 @@
+/* Fig. 10.2: fig10_02.c
+ * Using the structure members and 
+ * structure pointer operators */
+#include <stdio.h>
+
+/* card structure definition */
+struct card {
+	char *face; /* define pointer face */
+	char *suit; /* define pointer suit */
+}; /* end structure card */
+
+int main ( void )
+{
+	struct card aCard; /* define one struct  card variable */
+	struct card *cardPtr; /* define a pointer to a struct card */
+
+	/* place strings into aCard */
+	aCard.face = "Ace";
+	aCard.suit = "Spades";
+
+	cardPtr = &aCard; /* assign address of aCard to cardPtr */
+
+	printf( "%s%s%s\n%s%s%s\n%s%s%s\n", aCard.face, " of ", aCard.suit,
+		cardPtr->face, " of ", cardPtr->suit,
+		( *cardPtr ).face, " of ", ( *cardPtr ).suit );
+	return 0;
+} /* end main */
